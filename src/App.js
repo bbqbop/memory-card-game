479c558ccf0539ccf74942e44fac7412b84ef63d
bbqbop <dierk.peters@gmail.com>
2023-06-01 16:09:36 -0400

    add basic game logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useContext } from 'react';
+import GameOverContext from './components/gameOverContext';
+require.context('./icons', false, /\.(png|jpe?g|svg)$/)
 
-function App() {
+function importAll(r) {
+  let images = {};
+   r.keys().forEach((item, index) => { images[index] = r(item); });
+  return images
+ }
+const dinos = importAll(require.context('./icons', false, /\.(png|jpe?g|svg)$/));
+
+
+
+export const App = () => {
+  const [dinoSet, setDinoSet] = useState(createDinoSet(3))
+  const [order, setOrder] = useState([0,1,2])
+  const [gameOver, setGameOver] = useState(false);
+
+  function createDinoSet(num) {
+    const set = [];
+    while(set.length < num){
+      const index = Math.floor(Math.random() * Object.keys(dinos).length) 
+      if(set.includes(dinos[index])){
+        continue
+      } else {
+        set.push(dinos[index])
+      }
+    }
+    return set;
+  }
+
+  function shuffledIndices() {
+    const indices = [];
+    while(indices.length < dinoSet.length){
+      const index = Math.floor((Math.random() * dinoSet.length))
+      if(indices.includes(index)){
+        continue
+      } else {
+        indices.push(index);
+      }
+    }
+    setOrder(indices);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> 
+      <GameOverContext.Provider value={{ gameOver, setGameOver }}>
+        {gameOver ? <div>GAMEOVER</div> : 
+          order.map(idx => (
+           <Card src={dinoSet[idx]} shuffle={shuffledIndices} key={dinoSet.indexOf(dinoSet[idx])} />
+         ))}
+      </GameOverContext.Provider>
     </div>
-  );
+  )
 }
 
-export default App;
+function Card( {src, shuffle} ){
+  const [isClicked, setIsClicked] = useState(false);
+  const {setGameOver} = useContext(GameOverContext);
+
+  function handleClick(){
+    isClicked === false ? setIsClicked(true) : setGameOver(true);
+    shuffle();
+  }
+
+  return (
+    <div className="card">
+      <img src={src} onClick={handleClick} alt="Dino-Card"/>
+    </div>
+  )
+
+}
+
+
---
+++ src/components/gameOverContext.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const GameOverContext = React.createContext();
+
+export default GameOverContext;
